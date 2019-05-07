e535d9482b244c2b82f6a01b9d5817a36781d830
andrei.troanches <andrei.troanches@ATROANCHES-WIN>
2019-05-07 13:47:37 +0300

    fix(landing): add shared module

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LandingPageComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     LandingRoutingModule
   ]
 })
