f2d6f00b2f331d435b3389e8dec5de281456830c
andrei.troanches <andrei.troanches@ATROANCHES-WIN>
2019-05-07 13:23:58 +0300

    feat(core-shared): add core module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+const MODULES =  [
+  CommonModule
+];
+
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: MODULES,
+  exports: MODULES
 })
 export class SharedModule { }
