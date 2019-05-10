4385b7f96482b035e68d5c8b4274408b6d66617e
andrei.troanches <andrei.troanches@ATROANCHES-WIN>
2019-05-10 14:02:39 +0300

    partial creating form

--- src/app/features/festivals/festivals.module.ts
+++ src/app/features/festivals/festivals.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FestivalDetailsPageComponent } from './pages/festival-details-page/festival-details-page.component';
@@ -6,12 +8,14 @@ import { FestivalsRoutingModule } from './festivals-routing.module';
 import { FestivalService } from '../services/festival.service';
 import { FestivalListItemComponent } from './components/festival-list-item/festival-list-item.component';
 import { FestivalsListPageComponent } from './pages/festivals-list-page/festivals-list-page.component';
+import { FestivalFormComponent } from './pages/festival-form/festival-form.component';
 
 @NgModule({
-  declarations: [FestivalsListPageComponent, FestivalDetailsPageComponent, FestivalListItemComponent],
+  declarations: [FestivalsListPageComponent, FestivalDetailsPageComponent, FestivalListItemComponent, FestivalFormComponent],
   imports: [
     SharedModule,
     FestivalsRoutingModule,
+    FormsModule
   ],
   providers:[FestivalService]
 })
---
+++ src/app/features/festivals/pages/festival-form/festival-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fest-festival-form',
+  templateUrl: './festival-form.component.html',
+  styleUrls: ['./festival-form.component.scss']
+})
+export class FestivalFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
